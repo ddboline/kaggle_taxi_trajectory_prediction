f42d0667ee18b69cbd7b4593d777537389e601ff
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-12 08:13:40 -0400

    ...

--- compare_trajectories.pyx
+++ compare_trajectories.pyx
@@ -25,18 +25,18 @@ def haversine_distance(double lat1, double lon1, double lat2, double lon2):
     cdef double dlon = abs(lon1-lon2)*np.pi/180.
     lat1 *= np.pi/180.
     lat2 *= np.pi/180.
-    cdef double dist = 2. * r_earth * np.asin(np.sqrt(np.sin(dlat/2.)**2 +
+    cdef double dist = 2. * r_earth * np.arcsin(np.sqrt(np.sin(dlat/2.)**2 +
                                       np.cos(lat1)*np.cos(lat2)*np.sin(dlon/2.)**2))
     return dist
 
 def compare_trajectories(np.ndarray test_trj, np.ndarray train_trj):
     n_common = 0
-    for idx in test_trj.shape[0]:
+    for idx in range(test_trj.shape[0]):
         test_lat = test_trj[idx, 0]
         test_lon = test_trj[idx, 1]
         dlat, dlon = lat_lon_box(test_lat, test_lon, 0.2)
         n_common_tr = 0
-        for jdx in train_trj.shape[0]:
+        for jdx in range(train_trj.shape[0]):
             train_lat = train_trj[jdx, 0]
             train_lon = train_trj[jdx, 1]
             if abs(train_lat-test_lat) > dlat or \
