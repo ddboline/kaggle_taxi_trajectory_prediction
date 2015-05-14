20d59df31cf5e0f5bdccb9361d20957203542f7e
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-14 14:12:02 -0400

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+N=$1
 
 # for F in train.csv.gz test.csv.gz metaData_taxistandsID_name_GPSlocation.csv.gz;
 # do
@@ -13,11 +14,10 @@ mkdir -p train
 scp ddboline@ddbolineathome.mooo.com:/home/ddboline/setup_files/build/kaggle_taxi_trajectory_prediction/train/train_trj_*.csv.gz train/
 
 # time ./feature_extraction.py
-# ./load_data.py $1
+./load_data.py $N > output.out 2> output.err
 # ./my_model.py
 
 # D=`date +%Y%m%d%H%M%S`
 # tar zcvf output_${D}.tar.gz model.pkl.gz output.out output.err
 # scp model_*.pkl.gz ddboline@ddbolineathome.mooo.com:/home/ddboline/setup_files/build/kaggle_taxi_trajectory_prediction/
-ssh ddboline@ddbolineathome.mooo.com "~/bin/send_to_gtalk done_kaggle_taxi_trajectory_prediction"
-echo "JOB DONE taxi_trajectory_prediction"
+ssh ddboline@ddbolineathome.mooo.com "~/bin/send_to_gtalk done_kaggle_taxi_trajectory_prediction_$N"
