d628d527cc2344f50d4fc3a533d7260b2f2b9184
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-14 12:12:58 -0400

    ...

--- load_data.py
+++ load_data.py
@@ -157,6 +157,7 @@ def find_best_traj(do_plots=False, out_index=0):
         match_list_, min_n_match = get_matching_list(tidx, te_df=tedf_,
                                                      tr_df=train_nib,
                                                      skiplist=skiplist_)
+        print('match_list_', type(match_list_), min_n_match)
         print('match_list_', len(match_list_), min_n_match)
         match_list_parallel = [{} for i in range(100)]
         for tidx in match_list_:
