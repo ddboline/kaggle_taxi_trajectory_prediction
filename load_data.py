a18534cc65eeabd4322b2d23043c8b8b3cfa1328
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-14 16:36:23 -0400

    ...

--- load_data.py
+++ load_data.py
@@ -133,14 +133,15 @@ def find_best_traj(do_plots=False, out_index=0):
     csv_writer = csv.writer(outfile)
     csv_writer.writerow(outlabels)
     print(outfname, df_.shape, first_event, last_event)
-    for idx, row in df_.iterrows():
-        print(idx)
+    idx = 0
+    for _, row in df_.iterrows():
         if idx < first_event:
             continue
         if idx >= last_event:
             continue
         if idx % 10 == 0:
             print('test %d' % idx)
+        idx += 1
         tidx = row['TRAJECTORY_IDX']
         if is_test:
             tdf_ = test_trj
